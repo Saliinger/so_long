523023a8fa641faaea49504e707503c7671f8409
Saliinger <alexisnoukan@gmail.com>
2024-07-22 10:09:55 +0200

    norm

--- srcs/parsing/ft_valid.c
+++ srcs/parsing/ft_valid.c
@@ -6,7 +6,7 @@
 /*   By: anoukan <anoukan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/11 13:54:27 by anoukan           #+#    #+#             */
-/*   Updated: 2024/07/11 13:54:29 by anoukan          ###   ########.fr       */
+/*   Updated: 2024/07/22 10:09:30 by anoukan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,9 @@
 
 int	ft_valid(t_map *map)
 {
-	if (map->e == 1 && map->p == 1 && map->w == 1 && map->c >= 1 \
-        && map->notsquare == 1 && map->s >= 1)
+	if (map->e == 1 && map->p == 1 && map->w == 1 && map->c >= 1
+		&& map->notsquare == 1 && map->s >= 1)
 		return (1);
-    else
-	    return (ft_printf("parsing error\n map->e %d\n map->p %d\n map->w %d\n map->c %d\n map->notsquarre %d\n map->s %d\n",map->e, map->p, map->w, map->c, map->notsquare, map->s), 0);
+	else
+		return (0);
 }
--- srcs/parsing/parsing.c
+++ srcs/parsing/parsing.c
@@ -6,25 +6,25 @@
 /*   By: anoukan <anoukan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/23 10:51:58 by anoukan           #+#    #+#             */
-/*   Updated: 2024/07/11 13:51:22 by anoukan          ###   ########.fr       */
+/*   Updated: 2024/07/22 10:09:38 by anoukan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/so_long.h"
 
-static void free_map(t_map *map)
+static void	free_map(t_map *map)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while (map->map[i])
-    {
-        free(map->map[i]);
-        i++;
-    }
-    free(map->map);
-    free(map);
-    exit(EXIT_FAILURE);
+	i = 0;
+	while (map->map[i])
+	{
+		free(map->map[i]);
+		i++;
+	}
+	free(map->map);
+	free(map);
+	exit(EXIT_FAILURE);
 }
 
 void	ft_parsing(t_map *map, char **argv)
@@ -33,6 +33,6 @@ void	ft_parsing(t_map *map, char **argv)
 	map->w = ft_verify_wall(map);
 	ft_verify_element(map);
 	ft_not_square(map);
-    if (ft_valid(map) == 0)
-        free_map(map);
+	if (ft_valid(map) == 0)
+		free_map(map);
 }
