87a48710f43412ed72356d3c6355ea6aad417d49
Saliinger <88099624+Saliinger@users.noreply.github.com>
2024-07-22 10:07:02 +0200

    remove debug

--- srcs/parsing/ft_valid.c
+++ srcs/parsing/ft_valid.c
@@ -15,7 +15,7 @@
 int	ft_valid(t_map *map)
 {
 	if (map->e == 1 && map->p == 1 && map->w == 1 && map->c >= 1 \
-        && map->notsquare == 1 && map->s <= 1)
+        && map->notsquare == 1 && map->s >= 1)
 		return (1);
     else
 	    return (ft_printf("parsing error\n map->e %d\n map->p %d\n map->w %d\n map->c %d\n map->notsquarre %d\n map->s %d\n",map->e, map->p, map->w, map->c, map->notsquare, map->s), 0);
